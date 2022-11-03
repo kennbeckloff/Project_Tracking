7ea331ffe2f7e06a812743a6f80567c29fbf3d6e
kennbeckloff <kennbeckloff688@gmail.com>
2022-11-03 09:02:15 +0300

    front and backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,38 @@ class UsersController < ApplicationController
 
 
     def index
-      render json: User.all, status: :ok
+      render json: User.all, include: :cohort, status: :ok
     end
   
 
     def create
       user = User.create!(user_params)
       session[:user_id] = user.id
+      
       render json: user, status: :created
     end
   
     def show
       render json: @current_user
     end
+
+   def update
+        user = User.find(params[:id])
+        user.update!(user_params)
+        render json: user
+    end
+
+     # DELETE /users/1
+    def destroy
+        res = User.find(params[:id])
+        res.destroy
+        head :no_content
+    end
+    
   
     private
   
     def user_params
-      params.permit(:username, :password, :password_confirmation,:role, :cohort_id, :last_name)
+      params.permit(:username, :password, :password_confirmation,:role, :cohort_id)
     end
 end
